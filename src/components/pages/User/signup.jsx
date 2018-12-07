2508498d39b66860d8a36691901fe9458358fe96
handuo <handuo@dhcp-128-103-173-229.gsd.harvard.edu>
2018-12-07 16:41:42 -0500

    updated sign up page

--- src/components/pages/User/signup.jsx
+++ src/components/pages/User/signup.jsx
@@ -8,16 +8,34 @@ const genderOptions = [
 ]
 
 class SignUp extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: {
+                userName: "",
+                phone: "",
+                email: "",
+                password: ""
+            },
+            errors: {}
+        };
+    }
+
+
+
   render() {
+
     return (
     <div className="register-form clearfix">
      <Form size="big">
-      <Form.Field>
+      <Form.Field required>
           <label htmlFor="userName">User Name</label>
           <input
             type="text"
             id="userName"
             name="userName"
+            value={this.state.data.userName}
+            onChange={this.onRegistration}
             placeholder="please input your userName"
           />
         </Form.Field>
@@ -28,31 +46,38 @@ class SignUp extends React.Component {
             id="phone"
             name="phone"
             placeholder="please input your phone number"
+            value={this.state.data.phone}
+            onChange={this.onRegistration}
           />
         </Form.Field>
-        <Form.Field>
+        <Form.Field required>
           <label htmlFor="email">Email</label>
           <input
             type="email"
             id="email"
             name="email"
             placeholder="example@example.com"
+            value={this.state.data.email}
+            onChange={this.onRegistration}
           />
         </Form.Field>
-        <Form.Field>
+        <Form.Field required>
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
             name="password"
             placeholder="Make it secure"
+            value={this.state.data.password}
+            onChange={this.onRegistration}
           />
         </Form.Field>
         <Form.Field>
           <label htmlFor="gender">Gender</label>
           <Select placeholder='Select your gender' options={genderOptions} ></Select>
         </Form.Field>
-        <div  className="submit-button">
+        <Form.Checkbox label='Join FDS. By joining I accept all terms and conditions.' required />
+        <div className="submit-button">
           <Button primary>Join Us</Button>
         </div>
       </Form>
