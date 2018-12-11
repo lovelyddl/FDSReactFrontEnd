6bd7693273fc8ab5a4fcd1a6d7bdfc99f5243f4f
v_bwenlei <v_bwenlei@tencent.com>
2018-12-11 17:38:43 +0800

    clear unnecessary packages

--- src/api/user.js
+++ src/api/user.js
@@ -1,8 +1,8 @@
 import Axios from 'axios'
 Axios.defaults.withCredentials = true; // make ajax takes cookie
 
-// const host = "http://119.29.96.250:8080"
-const host = "http://localhost:8080"
+const host = "http://119.29.96.250:8080"
+// const host = "http://localhost:8080"
 
 export const userLogin = (userId, password) => {
   let url = host + "/users/login";
